23e7ede1fd4a0a78763ae1e016ccfdc7e0d2707d
Alex Barclay <adbarclay92@gmail.com>
2023-09-10 11:48:12 -0600

    Struggling with auth, moving to something else for now

--- src/main/kotlin/com/blincke/commune_api/configuration/WebSecurityConfiguration.kt
+++ src/main/kotlin/com/blincke/commune_api/configuration/WebSecurityConfiguration.kt
@@ -2,24 +2,28 @@ package com.blincke.commune_api.configuration
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer.withDefaults
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer.ExpressionInterceptUrlRegistry
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer
 import org.springframework.security.web.SecurityFilterChain
+
 
 @Configuration
 class SecurityConfiguration {
     @Bean
     @Throws(java.lang.Exception::class)
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        return http
+        http
             .authorizeHttpRequests { auth ->
                 auth
                     .anyRequest().authenticated()
             }
             .oauth2ResourceServer { oauth2 ->
                 oauth2.jwt {
-                    // This doesn't work
                 }
-            }.httpBasic(withDefaults()).build()
+            }
+        return http.build()
     }
 }
